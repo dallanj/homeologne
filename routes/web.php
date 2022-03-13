a45d8e7accae8614eec42caa419c124d03429a00
dallanj <dallanjones@pm.me>
2022-03-13 14:41:58 -0400

    added photo gallery to cms, and image upload functionality

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Livewire\Frontpage;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,10 +14,31 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::group(['middleware' => ['web']], function () {
+    Route::get('storage/{filename}', function ($filename) {
+        $userid = session()->get('user')->id;
+        return Storage::get($userid . '/' . $filename);
+    });
 });
 
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return view('dashboard');
 })->name('dashboard');
+
+Route::group(['middleware' =>[
+    'auth:sanctum',
+    'verified' 
+]], function () {
+ 
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+
+    Route::get('/gallery', function () {
+        return view('admin.gallery');
+    })->name('gallery');
+ 
+});
+ 
+ // Route::get('/{urlslug}', Frontpage::class);
+ Route::get('/', Frontpage::class);
